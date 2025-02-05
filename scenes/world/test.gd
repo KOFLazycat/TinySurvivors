d11fa3871308d37e6ece25b23c44b538aa5500e6
KOFLAzycat <lushoulei0552@163.com>
2025-02-05 10:58:45 +0800

    1. 修改资源

--- scenes/world/test.gd
+++ scenes/world/test.gd
@@ -2,7 +2,6 @@ class_name Test
 extends Node2D
 
 @onready var timer: Timer = $Timer
-@onready var damage_tscn: PackedScene = preload("res://scenes/vfx/damage_points.tscn")
 
 
 func _ready() -> void:
@@ -11,9 +10,4 @@ func _ready() -> void:
 
 
 func on_timer_timeout() -> void:
-	#Music.pause_resource.set_value(true)
-	var d: DamagePoints = damage_tscn.instantiate() as DamagePoints
-	d.set_displayed_points(100, false)
-	d.scale = Vector2(1, 1)
-	d.global_position = Vector2(960, 540)
-	add_child(d)
+	Music.pause_resource.set_value(true)
